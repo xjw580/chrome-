4aa8d8b854146453753d9a52cbf65e72defda2e9
zerg <xjw580@qq.com>
2021-10-12 23:13:25 +0800

    v1.0

---
+++ skipAd/js/content.js
@@ -0,0 +1,10 @@
+$(document).bind("keydown",function(param){
+	if(param.keyCode=="90"){
+		$(".bilibili-player-electric-panel-jump-content").trigger("click")
+	}
+})
+/* document.querySelector(".bilibili-player-electric-panel-jump").addEventListener("DOMNodeInserted",function(param){
+	console.log(param)
+	console.log("hello")
+})
+ */
